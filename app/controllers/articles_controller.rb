c460234057585e50939c3fbf8c235361afe03842
Guishny Desor <desorguishny@gmail.com>
2024-01-06 14:39:47 -0500

    Remove form to prevent users from adding new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,70 +1,16 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
-
   # GET /articles or /articles.json
   def index
     @articles = Article.all
-  end
-
-  # GET /articles/1 or /articles/1.json
-  def show
-  end
-
-  # GET /articles/new
-  def new
-    @article = Article.new
-  end
-
-  # GET /articles/1/edit
-  def edit
-  end
-
-  # POST /articles or /articles.json
-  def create
-    @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if params[:query].present?
+      @articles = Article.search_by_article_content(params[:query])
+      update_articles
     end
+    @articles = Article.all
   end
 
-  # PATCH/PUT /articles/1 or /articles/1.json
-  def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
+  def update_articles
+    render turbo_stream: turbo_stream.replace('articles', partial: 'articles/articles', locals: { articles: @articles })
   end
 
-  # DELETE /articles/1 or /articles/1.json
-  def destroy
-    @article.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
-end
